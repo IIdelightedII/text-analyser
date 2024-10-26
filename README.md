9cc196689b4339dd13f09a7bfef9310129500540
IIdelightedII <tastewatermelon105@gmail.com>
2024-10-26 18:23:25 +0500

    added readme and added words with max and min letters

---
+++ README.md
@@ -0,0 +1,2 @@
+# What it does 
+says frequency of words, number of different words, how many times different words used and more
--- main.py
+++ main.py
@@ -8,13 +8,17 @@ for char in punctuation:
 
 words = text.split()
 
-
+word_length = {}
 word_frequency = {}
 for i in range(len(words)):
     word_frequency[words[i]] = word_frequency.get(words[i], 0) + 1
+    if not words[i] in word_length:
+        word_length[words[i]] = len(words[i])
 
 # вывод результатов
 print("Количество разных слов:", len(set(words)))
+print(f"Слово с максимальным количеством букв: {max(word_length, key=word_length.get)}")
+print(f"Слово с минимальным количеством букв: {min(word_length, key=word_length.get)}")
 print("Частота слов:")
 for word, frequency in word_frequency.items():
     print(f"{word}: {frequency}")
